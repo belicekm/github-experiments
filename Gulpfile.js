f53cf05e25683198d949cf7a6e42e238b076a726
belicekm <belicek@gmail.com>
2016-08-31 15:56:54 +0200

    There will be used environment variable for GITHUB_TOKEN
    Signed-off-by: belicekm <belicek@gmail.com>

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,27 +2,16 @@
 var gulp = require('gulp');
 var release = require('gulp-github-release');
 var gulpUtil = require('gulp-util');
-var argv = require('yargs').argv;
-
-var token = argv.token;
-
-if (!token)
-{
-	throw new gulpUtil.PluginError({
-		plugin: 'deploy',
-		message: 'GitHub token was not specified!'
-    });
-}
 
 // js concat & minify
 gulp.task('release', function () {
 	gulp.src('./dist/deploy.zip')
 		.pipe(release({
-			token: token,                     // or you can set an env var called GITHUB_TOKEN instead
+			//token: token,                     // or you can set an env var called GITHUB_TOKEN instead
 			//owner: owner,                    // if missing, it will be extracted from manifest (the repository.url field)
 			//repo: 'publish-release',            // if missing, it will be extracted from manifest (the repository.url field)
-			tag: 'release-1.0.0',                      // if missing, the version will be extracted from manifest and prepended by a 'v'
-			name: 'publish release v1.0.0',     // if missing, it will be the same as the tag
+			tag: 'release-1.3.0',                      // if missing, the version will be extracted from manifest and prepended by a 'v'
+			name: 'publish release v1.3.0',     // if missing, it will be the same as the tag
 			notes: 'very good!',                // if missing it will be left undefined
 			draft: false,                       // if missing it's false
 			prerelease: false,                  // if missing it's false
@@ -30,6 +19,4 @@ gulp.task('release', function () {
 		}));
 });
 
-gulp.task('default', function () {
-	
-});
+gulp.task('default', function () {});
