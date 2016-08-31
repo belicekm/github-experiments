6567df6a40c3d4f6524249c433c918f0083e4d15
belicekm <belicek@gmail.com>
2016-08-31 16:01:56 +0200

    Added tag generation from Date.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,16 +2,17 @@
 var gulp = require('gulp');
 var release = require('gulp-github-release');
 var gulpUtil = require('gulp-util');
+var tag = new Date().toISOString().replace(/T.+$/, '').replace(/-/g, '.');
 
 // js concat & minify
 gulp.task('release', function () {
 	gulp.src('./dist/deploy.zip')
 		.pipe(release({
 			//token: token,                     // or you can set an env var called GITHUB_TOKEN instead
-			//owner: owner,                    // if missing, it will be extracted from manifest (the repository.url field)
-			//repo: 'publish-release',            // if missing, it will be extracted from manifest (the repository.url field)
-			tag: 'release-1.3.0',                      // if missing, the version will be extracted from manifest and prepended by a 'v'
-			name: 'publish release v1.3.0',     // if missing, it will be the same as the tag
+			//owner: owner,                    	// if missing, it will be extracted from manifest (the repository.url field)
+			//repo: 'publish-release',          // if missing, it will be extracted from manifest (the repository.url field)
+			tag: 'release-' + tag,              // if missing, the version will be extracted from manifest and prepended by a 'v'
+			name: 'publish release ' + tag,     // if missing, it will be the same as the tag
 			notes: 'very good!',                // if missing it will be left undefined
 			draft: false,                       // if missing it's false
 			prerelease: false,                  // if missing it's false
