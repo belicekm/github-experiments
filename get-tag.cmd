2a85b4699baa5facb4cc2c5011f859d5e55c107f
belicekm <belicek@gmail.com>
2016-09-02 10:15:05 +0200

    test

--- get-tag.cmd
+++ get-tag.cmd
@@ -1,2 +1,2 @@
 for /f %%i in ('git describe --tags --abbrev^=0 --match release-*') do @set TAG=%%i
-echo "##teamcity[setParameter name='env.GIT_LATEST_TAG' value='%%TAG%%']"
+echo "##teamcity[setParameter name='env.GIT_LATEST_TAG' value='12345']"
