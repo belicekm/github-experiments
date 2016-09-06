9545f3cd1739e415cee259161d6ea5f9aecbb7b1
belicekm <belicek@gmail.com>
2016-09-06 09:01:42 +0200

    Added task for git tag resolving using child_process .exec()

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,67 @@
 var gulp = require('gulp');
 var release = require('gulp-github-release');
 var gutil = require('gulp-util');
+var exec = require('child_process').exec;
 
-var TAG_ERR_TEXT = 'Unable to resolve GIT TAG from environment variable GIT_LATEST_TAG';
+var latestReleaseTag;
 
+gulp.task('gittag', function () {
+	return new Promise(function (resolve, reject) {
+		// get the latest tag with "release-" prefix
+		var command = 'git describe --tags --abbrev^=0 --match release-*';
+		exec(command, function (error, stdout, stderr) {
+			var outText = '' + stdout,
+				errText = '' + stderr;
+
+			if (error || errText || !outText)
+			{
+				reject();
+			}
+			else
+			{
+				latestReleaseTag = outText.trim();
+				if (latestReleaseTag)
+				{
+					resolve();
+				}
+				else
+				{
+					gutil.log(gutil.colors.red('GIT tag not found - aborting!'));
+					reject();
+				}
+			}
+		});
+	});
+});
+
+gulp.task('release:novar', ['gittag'], function (callback) {
+	gutil.log('Using release tag: ' + latestReleaseTag);
+	if (!process.env.GITHUB_TOKEN)
+	{
+		callback('Unable to resolve GitHub authentication token from environment variable GITHUB_TOKEN');
+	}
+	else
+	{
+		return gulp
+				.src('./dist/deploy.zip')
+				.pipe(release({
+					tag: latestReleaseTag,
+					name: 'publish release ' + latestReleaseTag,     // if missing, it will be the same as the tag
+					notes: 'very good!',                // if missing it will be left undefined
+					draft: false,                       // if missing it's false
+					prerelease: false,                  // if missing it's false
+					manifest: require('./package.json') // package.json from which default values will be extracted if they're missing
+				}));
+	}
+});
+
+// depends on environment variable set from tag.bat
 gulp.task('release', function (callback) {
 	var latestTag = process.env.GIT_LATEST_TAG;
+	if (!process.env.GITHUB_TOKEN)
+	{
+		callback('Unable to resolve GitHub authentication token from environment variable GITHUB_TOKEN');
+	}
 	if (latestTag)
 	{
 		gutil.log('Using release tag: ' + latestTag);
@@ -23,7 +79,7 @@ gulp.task('release', function (callback) {
 	}
 	else
 	{
-		callback(TAG_ERR_TEXT);
+		callback('Unable to resolve GIT tag from environment variable GIT_LATEST_TAG');
 	}
 });
 
