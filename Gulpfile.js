e25e29b79e51d17cf0eae19e526c7e10e7a5ac53
belicekm <belicek@gmail.com>
2016-09-06 12:19:03 +0200

    Added some comments. Added explicit passing of GITHUB_TOKEN to gulp-github-release.

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ var exec = require('child_process').exec;
 
 var latestReleaseTag;
 
+// get latest GIT tag which has "release-" prefix and store it in latestReleaseTag variable.
 gulp.task('gittag', function () {
 	return new Promise(function (resolve, reject) {
 		// get the latest tag with "release-" prefix
@@ -35,17 +36,16 @@ gulp.task('gittag', function () {
 	});
 });
 
+// Perform GitHub release using public GitHub API
+// Depends on GITHUB_TOKEN
 gulp.task('release:novar', ['gittag'], function (callback) {
 	gutil.log('Using release tag: ' + latestReleaseTag);
-	if (!process.env.GITHUB_TOKEN)
-	{
-		callback('Unable to resolve GitHub authentication token from environment variable GITHUB_TOKEN');
-	}
-	else
+	if (process.env.GITHUB_TOKEN)
 	{
 		return gulp
 				.src('./dist/deploy.zip')
 				.pipe(release({
+					token: process.env.GITHUB_TOKEN,
 					tag: latestReleaseTag,
 					name: 'publish release ' + latestReleaseTag,     // if missing, it will be the same as the tag
 					notes: 'very good!',                // if missing it will be left undefined
@@ -54,9 +54,16 @@ gulp.task('release:novar', ['gittag'], function (callback) {
 					manifest: require('./package.json') // package.json from which default values will be extracted if they're missing
 				}));
 	}
+	else
+	{
+		callback('Unable to resolve GitHub authentication token from environment variable GITHUB_TOKEN');
+	}
 });
 
-// depends on environment variable set from tag.bat
+// ANOTHER VARIANT:
+
+// Perform GitHub release using public GitHub API
+// Depends on environment variable GITHUB_TOKEN (hardcoded in TeamCity) and GIT_LATEST_TAG (set from tag.bat)
 gulp.task('release', function (callback) {
 	var latestTag = process.env.GIT_LATEST_TAG;
 	if (!process.env.GITHUB_TOKEN)
